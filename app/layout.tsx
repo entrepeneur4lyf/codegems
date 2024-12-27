76e3e63b89f7969b28f3c6871d709c81bd3e11bb
bebedi15 <70455072+bebedi15@users.noreply.github.com>
2024-12-27 22:19:20 +0100

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,12 @@ import Footer from '@/components/Footer';
 export const metadata: Metadata = {
   title: 'Code Gems',
   description: 'Discover remarkable GitHub projects',
-}
+  keywords: 'github, projects, code, gems, programming',
+  icons: {
+    icon: 'https://cdn.discordapp.com/attachments/1019356251106324496/1322312765750120529/page-removebg-preview.png?ex=67706b3a&is=676f19ba&hm=70bc100b33421611e3a10e036428833a8d3baeb85d251331a2104b76a169c08d&'
+  },
+};
+
 
 export default function RootLayout({
   children,
@@ -16,7 +21,6 @@ export default function RootLayout({
 }) { 
   return (
     <html lang="en">
-      
       <body>
       <Navbar />
         <SavedProvider>
