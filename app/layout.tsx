6a3225594565d76281c240bddabe077117bcec97
bebedi15 <70455072+bebedi15@users.noreply.github.com>
2025-01-06 09:31:13 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,22 @@ import { Analytics } from "@vercel/analytics/react"
 export const metadata: Metadata = {
   title: 'Code Gems',
   description: 'Discover remarkable GitHub projects',
-  keywords: 'github, projects, code, gems, programming',
+  keywords: ['github', 'projects', 'code', 'gems', 'programming'],
   icons: {
-    icon: 'https://cdn.discordapp.com/attachments/1019356251106324496/1322312765750120529/page-removebg-preview.png?ex=67706b3a&is=676f19ba&hm=70bc100b33421611e3a10e036428833a8d3baeb85d251331a2104b76a169c08d&'
+    icon: [
+      {
+        url: 'https://cdn.discordapp.com/attachments/1019356251106324496/1322312765750120529/page-removebg-preview.png?ex=67706b3a&is=676f19ba&hm=70bc100b33421611e3a10e036428833a8d3baeb85d251331a2104b76a169c08d&',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+    ],
   },
-};
+  openGraph: {
+    title: 'Code Gems',
+    description: 'Discover remarkable GitHub projects',
+    type: 'website',
+  },
+}
 
 
 export default function RootLayout({
