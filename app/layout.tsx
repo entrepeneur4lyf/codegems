6692cec69beecd2dbdd0a2c41955f311ab2b4a8b
bebedi15 <70455072+bebedi15@users.noreply.github.com>
2025-01-06 12:30:28 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { SavedProvider } from './saved/SavedContext'
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { Analytics } from "@vercel/analytics/react"
-import Script from 'next/script'
+
 
 export const metadata: Metadata = {
   title: 'Code Gems',
@@ -42,7 +42,6 @@ export default function RootLayout({
           </SavedProvider>
           <Footer />
           <Analytics />
-         <Script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="bebedi" data-description="Support me on Buy me a coffee!" data-message="" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></Script>
       </body>
     </html>
   )
