d119116a93963258cb5c05c2c57693c8180ad210
bebedi15 <bebedi15@gmail.com>
2025-01-07 09:10:45 +0100

    Fixed Website Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: 'https://media.discordapp.net/attachments/1019356251106324496/1322312765750120529/page-removebg-preview.png?ex=677c48ba&is=677af73a&hm=4e2d042a1355aa5cd578ef86a0c4a81a49f7de1d35b72dc64885df23d8778bc3&=&format=webp&quality=lossless',
+        url: '/icon.png',
         sizes: '32x32',
         type: 'image/png',
       },
