a6c13f525413d97eec484845371a455b4e0846f8
Bebedi <70455072+bebedi15@users.noreply.github.com>
2025-01-08 15:27:35 +0000

    removed discordpromo import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import GithubProjectsPage from '@/components/GithubProjectsPage'
-import DiscordPromo from '@/components/DiscordPromo';  
 
 export default function Home() {
   return <GithubProjectsPage />
